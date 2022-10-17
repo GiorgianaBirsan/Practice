13748565e068a37e55a17323ee541ca389bd81d5
Giorgiana Bîrsan <birsangiorgiana@gmail.com>
2022-10-17 23:35:07 +0300

    [17/10] commit

---
+++ Submit Button/script.js
@@ -0,0 +1,13 @@
+const formPassword = document.querySelector("#password");
+const btnLogin = document.querySelector("#btn-login");
+
+password.textContent = "proiect123"
+    //console.log(password.textContent);
+
+function rightMove(x) {
+    x.style.marginRight = "-200px";
+};
+
+function leftMove(x) {
+    x.style.marginRight = "200px";
+};
