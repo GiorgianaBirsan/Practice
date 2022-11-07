6e42d6da255ba4dfae1a3145ddd71bd856a90db0
Giorgiana Bîrsan <birsangiorgiana@gmail.com>
2022-11-07 02:17:06 +0200

    7/11 update code

--- Submit Button/script.js
+++ Submit Button/script.js
@@ -1,9 +1,10 @@
 const btnLogin = document.querySelector("#btn-login");
+
 var position;
-const password = "proiect123";
+const password = "123";
+
 
 let formPassword = document.getElementById("password").addEventListener("input", () => {
-    console.log(`document.getElementById("password ").value`, document.getElementById("password").value);
     if (document.getElementById("password").value !== password) {
         btnLogin.addEventListener("mouseover", function() {
             position ? (position = 0) : (position = 150);
@@ -13,19 +14,29 @@ let formPassword = document.getElementById("password").addEventListener("input",
 
         document.getElementById("password").style.borderColor = "red";
         btnLogin.style.background = "red";
+        btnLogin.style.borderColor = "red";
+        btnLogin.style.color = "white";
+
+
     } else {
+
         btnLogin.style.transform = `translate(0px,0px)`;
         btnLogin.addEventListener("mouseout", function() {
             position = 0;
             btnLogin.style.transform = `translate(${position}px,0px)`;
-
         });
         btnLogin.addEventListener("mouseover", function() {
             position = 0;
             btnLogin.style.transform = `translate(${position}px,0px)`;
-
         });
         document.getElementById("password").style.borderColor = "green";
         btnLogin.style.background = "green";
+        btnLogin.style.borderColor = "green";
+        btnLogin.style.color = "white";
+
+        btnLogin.addEventListener('click', () => {
+            alert("Yes! The password is 123")
+        })
+
     }
 });
